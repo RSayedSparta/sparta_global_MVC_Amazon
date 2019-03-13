bb5c4e03c3728e43861f875100714dd5f9c707f7
rahib <rsayed@spartaglobal.com>
2019-03-13 02:07:46 +0000

    Set out the main basic pages of the amazon website

---
+++ AmazonMVC/AmazonMVC/Controllers/AmazonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmazonMVC.Controllers
+{
+    public class AmazonController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
